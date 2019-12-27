28ac46e962aef1bdd16b4632f9189b2a7ff0409f
Medinho89 <mohamedtaiebbsf@yahoo.com>
2019-12-27 17:14:33 +0100

    Updates

---
+++ precache-manifest.a5cf532c56550e45341306f87dce861b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566800fb38713f439672335be0e6832d",
+    "url": "/HangmanGame-react/index.html"
+  },
+  {
+    "revision": "4926596d418b3855be86",
+    "url": "/HangmanGame-react/static/css/2.eb395e31.chunk.css"
+  },
+  {
+    "revision": "ef421b84d11c1b35b2a0",
+    "url": "/HangmanGame-react/static/css/main.588f5233.chunk.css"
+  },
+  {
+    "revision": "4926596d418b3855be86",
+    "url": "/HangmanGame-react/static/js/2.f096e99d.chunk.js"
+  },
+  {
+    "revision": "da01034c70d479d890a70d42d48c6305",
+    "url": "/HangmanGame-react/static/js/2.f096e99d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef421b84d11c1b35b2a0",
+    "url": "/HangmanGame-react/static/js/main.8881e27f.chunk.js"
+  },
+  {
+    "revision": "2004460d270aa3f9f118",
+    "url": "/HangmanGame-react/static/js/runtime-main.2da5c2ea.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/HangmanGame-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/HangmanGame-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/HangmanGame-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/HangmanGame-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/HangmanGame-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
